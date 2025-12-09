aa7046a76fc38f4680c29680e282a939c2da9b20
bdbdevi-cyber <bdbdevi@gmail.com>
2025-12-09 22:15:08 +0530

    push origin

--- myshopp/settings.py
+++ myshopp/settings.py
@@ -17,12 +17,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'myshopp.urls'
@@ -83,4 +85,11 @@ UPI_NAME = "My Shop"
 
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "makeup.com"]
+
+
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
